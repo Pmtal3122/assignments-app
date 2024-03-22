9861b17eb09e3645ae9787aca945a9f2c5894e75
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-03-22 15:49:59 +0530

    Question inserted draft 1

---
+++ server/assignmentsServer.js
@@ -0,0 +1,57 @@
+const { Client } = require('pg');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'Piyush@2002',
+    port: 5432,
+    database: 'assignmentsdb'
+});
+
+app.post('/insertQuestion', (req, res) => {
+    console.log("Inside insertQuestion");
+    console.log(req.body);
+    const { questionText, questionMarks } = req.body;
+
+    let qid = Math.floor(Math.random() * 100);
+
+    client.connect((err, res) => {
+        if (!err) console.log("Connected successfully");
+        else console.log("Connection failed");
+    })
+
+    let repeatCheck = false;
+    while(repeatCheck === false) {
+        client.query(`select * from questions where qid = ${qid}`, (err, res) => {
+            if(!err) {
+                if(res.rows === 0) repeatCheck = true;
+                else qid = Math.floor(Math.random() * 100);
+            }
+        })
+    }
+
+    const queryInsert = `insert into questions values (${qid}, '${questionText}', ${Number(questionMarks)})`;
+
+    client.query(queryInsert, (err, res) => {
+        if(!err) {
+          console.log("Insertion Successful");
+          client.query('select * from questions', (error, resp) => {
+            if(!error) console.log(resp.rows);
+            else console.log("Fetching values from table unsuccessful");
+          })
+        }
+        else console.log("Insertion unsuccessful");
+      })
+
+    return "insertQuestion Exited";
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+  async function submitTest(event) {
+    event.preventDefault();
+    console.log(event.target);
+
+    const formData = new FormData(event.target);
+
+    const formDataJson = {};
+    formData.forEach((value, key) => {
+      formDataJson[key] = value;
+    })
+
+    console.log(formDataJson);
+
+    await axios.post("http://localhost:3000/insertQuestion", formDataJson)
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+  }
+
+  return (
+    <>
+    <h1>Add a question</h1>
+    <form action="" onSubmit={(event) => submitTest(event)}>
+      <label htmlFor="questionText">Question: </label>
+      <input type="text" name="questionText" id="questionText" autoComplete="false" />
+      <br />
+      <label htmlFor="questionMarks">Marks: </label>
+      <input type="number" name="questionMarks" id="questionMarks" />
+      <br />
+      <input type="submit" value="submitQuestion" />
+    </form>
+    </>
+  );
+}
+
+export default App;
