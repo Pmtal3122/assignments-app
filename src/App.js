02dd3aea55438314703f0c370e8230414f395290
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-03-22 16:43:47 +0530

    Reset question form after submission

--- src/App.js
+++ src/App.js
@@ -17,22 +17,26 @@ function App() {
     console.log(formDataJson);
 
     await axios.post("http://localhost:3000/insertQuestion", formDataJson)
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+      .then((res) => {
+        console.log(res);
+        event.target.questionText.value = "";
+        event.target.questionMarks.value = 0;
+      })
+      .catch((err) => console.error(err));
   }
 
   return (
     <>
-    <h1>Add a question</h1>
-    <form action="" onSubmit={(event) => submitTest(event)}>
-      <label htmlFor="questionText">Question: </label>
-      <input type="text" name="questionText" id="questionText" autoComplete="false" />
-      <br />
-      <label htmlFor="questionMarks">Marks: </label>
-      <input type="number" name="questionMarks" id="questionMarks" />
-      <br />
-      <input type="submit" value="submitQuestion" />
-    </form>
+      <h1>Add a question</h1>
+      <form action="" onSubmit={(event) => submitTest(event)}>
+        <label htmlFor="questionText">Question: </label>
+        <input type="text" name="questionText" id="questionText" autoComplete="false" />
+        <br />
+        <label htmlFor="questionMarks">Marks: </label>
+        <input type="number" name="questionMarks" id="questionMarks" />
+        <br />
+        <input type="submit" value="submitQuestion" />
+      </form>
     </>
   );
 }
